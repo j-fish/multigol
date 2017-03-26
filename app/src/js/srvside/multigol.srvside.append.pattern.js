cb8e4e7e0cebb0dadc9c3e17eacc1391a0bbe871
thw <thw@thwcomp>
2017-03-26 21:44:50 +0200

    sanitize pattern input

--- app/src/js/srvside/multigol.srvside.append.pattern.js
+++ app/src/js/srvside/multigol.srvside.append.pattern.js
@@ -7,20 +7,22 @@ exports.append = function(gol, data, sanitizer) {
 	var pattern = appendData.pattern.split('~');
 	var xy; 
 
+    var mousex = sanitizer.perform(appendData.mousex);
+    var mousey = sanitizer.perform(appendData.mousey);
+    var color = sanitizer.perform(appendData.color);
+    var zx = sanitizer.perform(appendData.zonex);
+    var zy = sanitizer.perform(appendData.zoney);
+    var gw = sanitizer.perform(appendData.gridW);
+    var gh = sanitizer.perform(appendData.gridH);
+    var cli = sanitizer.perform(appendData.client);
+    var csize = sanitizer.perform(appendData.cellSize);
+
 	for (var i = 0; i < pattern.length; i++) {    
         xy = pattern[i].split('$');
-        gol.iORunLibInput(
-            appendData.mousex, 
-            appendData.mousey, 
-            xy[0], 
-            xy[1], 
-        	appendData.color, 
-            appendData.zonex, 
-            appendData.zoney, 
-        	appendData.gridW, 
-            appendData.gridH, 
-            appendData.client, 
-        	appendData.cellSize
+        gol.iORunLibInput(mousex, mousey, 
+            sanitizer.perform(xy[0]), 
+            sanitizer.perform(xy[1]), 
+        	color, zx, zy, gw, gh, cli, csize
         );
     }
 
